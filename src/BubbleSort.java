dc3c33015ae48ef787af058ecb4ba909f976355c
rickvanw <rickvanweersel@hotmail.com>
2016-09-08 14:45:33 +0200

    laatste

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -5,11 +5,19 @@ import java.util.Random;
 /**
  * Created by rickv on 1-9-2016.
  */
-public class BubbleSortExample {
-    public static void bubbleSort(int[] arr) {
+public class BubbleSort implements Runnable{
 
-        int n = arr.length;
-        int temp = 0;
+    private int n;
+    private int temp = 0;
+    private int[] arr;
+
+    public BubbleSort(int[] arr) {
+        n = arr.length;
+        this.arr = arr;
+    }
+
+    @Override
+    public void run() {
         for(int i=0; i < n; i++){
             for(int j=1; j < (n-i); j++){
                 if(arr[j-1] > arr[j]){
@@ -22,7 +30,6 @@ public class BubbleSortExample {
             }
         }
     }
-
 }
 
 
